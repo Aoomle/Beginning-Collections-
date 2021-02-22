c4977a664486b5852f7f122a3913252dad60c4b5
Abdulmalik Muhammad <bawasuru@gmail.com>
2021-02-22 23:22:04 +0100

    Initial Commit

---
+++ Part 2 - (ALL EPISODES) Beginning Collections - Begin.playground/Pages/02 - Tuples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [⇐ Previous: 01 - Introduction](@previous)
+//: ## Episode 02: Tuples
+
+let student: (name: String, age: Int) = ("Abdulmalik", 27)
+student.name
+student.age
+
+let studentData = (name: "Bobo", age: 27, petName: "Mango")
+
+studentData.name
+//: [⇒ Next: 03 - Challenge - Tuples](@next)
+
